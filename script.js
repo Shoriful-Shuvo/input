913870566f87bd3aa025a9940465b4812a0230f6
Shoriful-Shuvo <shuvo696727xyz@gmail.com>
2024-08-27 19:04:52 +0600

    develop

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ let btn = document.querySelector("button")
  let ressult = document.querySelector("h1") 
 
 
- btn,addEventListener("click",function () {
+ btn.addEventListener("click",function () {
     if (age.value >= 18) {
         ressult.innerHTML = "tmi biye koro"   
-        btn.addEventListener("click", function () {
-            ressult.style.color = "white"
-            ressult.style.background = "red"      
-          })
-        }
-    else {
        
-        ressult.innerHTML = "tmi choto"
-        btn.addEventListener("click", function () {
-            ressult.style.color = "white"
-            ressult.style.background = "green"
+            ressult.style.color = "white";
+            ressult.style.background = "red";      
+         
+        }
+ 
+        else{
+            ressult.innerHTML = "tmi choto"
+       
+            ressult.style.color = "white";
+            ressult.style.background = "green";
 
-        }) 
-    }
+        }
  })
